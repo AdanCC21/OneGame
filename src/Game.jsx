34529d79f7fb81e0e7b46a5afaffc6f8c66fc702
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2025-01-07 20:33:52 -0800

    eventos comunes

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,13 +1,14 @@
-import { act, useState } from "react";
+import { act, useEffect, useState } from "react";
 
 export function Game({ players }) {
     // Vivos [[url,name,level,true]] level=probabilidad de matar 1...4, true = vivo o muerto
     let [activePlayers, setActive] = useState(players.map((current) => { return [...current] }));
     // relaciones, [[j1,j2,type]], type : 1 = relacion, 0 = trato;
     let [relation, setRelation] = useState([]);
-
     // impar dia, par = noche
     let [round, setRound] = useState(2);
+
+    let [eventsReg, setEvents] = useState([]);
 
     function killSelf(self) {
         setActive(activePlayers.map((actual) => {
@@ -23,11 +24,11 @@ export function Game({ players }) {
         let dif = true;
         let ind = 0;
         let history = [];
-    
-        while (band && ind < activePlayers.length) {
+
+        while (band && ind <= activePlayers.length) {
             dif = true;
             let random = Math.floor(Math.random() * activePlayers.length);
-            
+
             // Verificar si ya se ha elegido ese jugador
             for (let i = 0; i < history.length; i++) {
                 if (history[i] === random) {
@@ -35,12 +36,12 @@ export function Game({ players }) {
                     break;
                 }
             }
-    
+
             if (history.length === activePlayers.length) {
                 console.log("Todos los jugadores muertos");
                 break;
             }
-    
+
             if (dif) {
                 // Verificar si el jugador está muerto
                 if (activePlayers[random][3] === false) {
@@ -69,9 +70,54 @@ export function Game({ players }) {
         }
     }
 
+    function SelectDuo(current) {
+        // FALTA VERIFICAR QUE EL QUE SEA SELECCIONADO NO ESTE EN UNA RELACION O TRATO
+        let dif = true;
+        let ind = 0;
+        let history = [];
+
+        while (ind <= activePlayers.length) {
+            dif = true;
+            let random = Math.floor(Math.random() * activePlayers.length);
+
+            // Verificar si ya se ha elegido ese jugador
+            for (let i = 0; i < history.length; i++) {
+                if (history[i] === random) {
+                    dif = false;
+                    break;
+                }
+            }
+
+            if (history.length === activePlayers.length) {
+                console.log("Todos los jugadores muertos");
+                break;
+            }
+
+            if (dif) {
+                // Verificar si el jugador está muerto
+                if (activePlayers[random][3] === false) {
+                    console.log('Está muerto el target: ' + activePlayers[random][1]);
+                    history.push(random);
+                    ind++;
+                } else {
+                    // Verificar si no es el mismo jugador
+                    if (activePlayers[random] !== current) {
+                        // Retornar duo
+                        return activePlayers[random];
+                    } else {
+                        console.log('Es el mismo, cambiando...');
+                        history.push(random);
+                        ind++;
+                    }
+                }
+            }
+        }
+    }
+
     function event(current) {
         if (current[3] === false) {
             console.log('jugador muerto')
+            return false;
         }
         else {
             // Evento
@@ -83,11 +129,13 @@ export function Game({ players }) {
                 if (random >= 70 && random <= 90) {
                     killSelf(current);
                     console.log(`murio ${current[0]}`)
+                    return [current, 'death'];
                 }
                 else {
                     // Comun
                     if (random >= 20 && random <= 70) {
                         console.log(`comun ${current[0]}`)
+                        return [current, 'comun']
                     }
                 }
             }
@@ -96,15 +144,20 @@ export function Game({ players }) {
                 if (random > 90 && random <= 100) {
                     // a quien matamos
                     let killed = KillSomeone(current);
+                    return [current, 'kill', killed]
                 } else {
                     // Trato
                     if (random > 5 && random < 20) {
-                        console.log(`Trato ${current[0]}`)
+                        let duo = SelectDuo(current);
+                        console.log(`Trato ${current[0]} with ${duo}`)
+                        return [current, 'deal', duo]
                     }
                     else {
                         // Relacion
                         if (random >= 0 && random < 5) {
-                            console.log(`Relacion ${current[0]}`)
+                            let duo = SelectDuo(current);
+                            console.log(`Relacion ${current[0]} with ${duo}`)
+                            return [current, 'relation', duo]
                         }
                     }
                 }
@@ -112,33 +165,37 @@ export function Game({ players }) {
         }
     }
 
-    
-    let [playerTurn, setTurn] = useState(0);
-    
+    useEffect(() => {
+        start();
+    },[])
+
     const start = () => {
-        if (round % 2 == 0) {// Dia
-            console.log(playerTurn)
-            let current = activePlayers[playerTurn];
-            if (current != null || current != undefined) {
-                event(current)
-                
-                // Si ya paso todos los jugadores
-                if(playerTurn >=players.length-1){
-                    setTurn(0)
-                }else{
-                    setTurn(playerTurn+1);
+        let tempEventReg = [];
+        for (let i = 0; i < activePlayers.length; i++) {
+            let current = activePlayers[i];
+            if (current != null) {
+                let ok = event(current);
+                if (ok !== false) {
+                    tempEventReg.push(ok);
                 }
             }
         }
+        setEvents(tempEventReg); 
     }
-
 
     return (
         <div>
             <h1>hola</h1>
-            <button onClick={() => {
+            {eventsReg.map((current, index) => (
+                <div key={index}>
+                    <img style={{ width: 200 }} src={current[0]} alt={current[1]} />
+                    <h2>{`${current[1]} mensaje`}</h2>
+                </div>
+            ))}
+            {/* {start()} */}
+            {/* <button onClick={() => {
                 start()
-            }}>Test</button>
+            }}>Test</button> */}
         </div>
     );
 }
